93623ad0ebaee67186da09ed45fa9dffc53d16e5
thomas-young-2013 <1225646303@qq.com>
2019-12-22 22:32:44 +0100

    update

--- mfes/facade/mfse.py
+++ mfes/facade/mfse.py
@@ -17,7 +17,7 @@ from mfes.config_space import convert_configurations_to_array, sample_configurat
 class MFSE(BaseFacade):
 
     def __init__(self, config_space: ConfigurationSpace, objective_func, R,
-                 num_iter=10000, eta=3, p=0.5, n_workers=1, random_state=1,
+                 num_iter=10000, eta=3, p=0.3, n_workers=1, random_state=1,
                  init_weight=None, update_enable=True, weight_method='rank_loss_softmax',
                  multi_surrogate=True, fusion_method='gpoe'):
         BaseFacade.__init__(self, objective_func, n_workers=n_workers)
@@ -260,6 +260,7 @@ class MFSE(BaseFacade):
                     sampled_y = np.random.normal(mean_list[idx], var_list[idx])
                     _num = self.calculate_preserving_order_num(sampled_y, test_y)
                     order_preseving_nums.append(_num)
+
                 # For basic surrogate i=K. cv
                 if len(test_y) < 10:
                     order_preseving_nums.append(0)
