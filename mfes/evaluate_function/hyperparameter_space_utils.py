40eff051f5f4d186b5c4278e4f271ec6bb7de390
shenyu <459240868@qq.com>
2020-01-08 21:51:41 +0800

    updage convnet training

--- mfes/evaluate_function/hyperparameter_space_utils.py
+++ mfes/evaluate_function/hyperparameter_space_utils.py
@@ -1,6 +1,7 @@
 from ConfigSpace.configuration_space import ConfigurationSpace
 from ConfigSpace.hyperparameters import UniformFloatHyperparameter, \
     UniformIntegerHyperparameter, CategoricalHyperparameter
+from ConfigSpace.conditions import InCondition
 
 
 def get_benchmark_configspace(benchmark_id):
@@ -44,6 +45,34 @@ def get_benchmark_configspace(benchmark_id):
                                                   log=True)
         momentum = UniformFloatHyperparameter("momentum", 0.5, .99, default_value=0.9)
         cs.add_hyperparameters([padding_size, batch_size, init_lr, lr_decay_factor, weight_decay, momentum])
+    elif benchmark_id=='convnet':
+        cs = ConfigurationSpace()
+
+        learning_rate = UniformFloatHyperparameter("learning_rate", 1e-5, 5e-2, default_value=1e-4, q=3e-5, log=True)
+        batch_size = UniformIntegerHyperparameter("batch_size", 16, 128, q=16, default_value=32)
+        momentum = UniformFloatHyperparameter("momentum", 0., .5, default_value=0., q=.1)
+        lr_decay = UniformFloatHyperparameter("lr_decay", .7, .99, default_value=9e-1, q=3e-2)
+        dropout_value = UniformFloatHyperparameter("dropout", .1, .7, default_value=.5, q=.1)
+        cs.add_hyperparameters([learning_rate, batch_size, momentum, lr_decay, dropout_value])
+
+        num_pooling_layer = UniformIntegerHyperparameter("n_pooling_layer", 2, 3, default_value=2)
+        num_conv_layer1 = UniformIntegerHyperparameter("n_conv_layer1", 16, 64, default_value=32, q=2)
+        num_conv_layer2 = UniformIntegerHyperparameter("n_conv_layer2", 32, 96, default_value=64, q=2)
+        num_conv_layer3 = UniformIntegerHyperparameter("n_conv_layer3", 32, 96, default_value=64, q=2)
+        num_fully_layer = UniformIntegerHyperparameter("n_fully_unit", 128, 512, default_value=256, q=64)
+        cs.add_hyperparameters([num_pooling_layer, num_conv_layer1, num_conv_layer2, num_conv_layer3, num_fully_layer])
+        for i in [1, 2, 3]:
+            kernel_init_stddev = UniformFloatHyperparameter(
+                "kernel_init_stddev%d" % i, 1e-3, 5e-2, default_value=1e-2, q=2e-3)
+            kernel_regularizer = UniformFloatHyperparameter(
+                "kernel_regularizer%d" % i, 1e-9, 1e-4, default_value=1e-6, q=5e-7, log=True)
+            cs.add_hyperparameters([kernel_init_stddev, kernel_regularizer])
+            if i == 3:
+                k_init_cond = InCondition(child=kernel_init_stddev, parent=num_pooling_layer, values=[3])
+                k_reg_cond = InCondition(child=kernel_regularizer, parent=num_pooling_layer, values=[3])
+                cs.add_conditions([k_init_cond, k_reg_cond])
+
+        return cs
     else:
         raise ValueError('Invalid benchmark id: %s!' % benchmark_id)
     return cs
