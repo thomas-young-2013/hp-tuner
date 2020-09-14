dcbbf06f14b96a3354f995f39a50df4c4431c420
shenyu <459240868@qq.com>
2020-09-14 15:17:03 +0800

    update resnet

--- mfes/evaluate_function/resnet/cifar10_input.py
+++ mfes/evaluate_function/resnet/cifar10_input.py
@@ -1,36 +1,153 @@
 import numpy as np
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import cv2
 
-import torch
-from torch.utils.data import DataLoader, SubsetRandomSampler
-import torchvision
-import torchvision.transforms as transforms
+IMG_WIDTH = 32
+IMG_HEIGHT = 32
+IMG_DEPTH = 3
+NUM_CLASS = 10
 
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+TRAIN_RANDOM_LABEL = False  # Want to use random label for train data?
+VALI_RANDOM_LABEL = False  # Want to use random label for validation?
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+EPOCH_SIZE = 40000
 
-full_dataset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-dataset_size = len(full_dataset)
-indices = list(range(dataset_size))
-split = int(np.floor(0.2 * dataset_size))
-np.random.seed(1)
-np.random.shuffle(indices)
-train_indices, val_indices = indices[split:], indices[:split]
+(x_all, y_all), (x_test, y_test) = cifar10.load_data()
+x_all = x_all.astype('float32')
+x_test = x_test.astype('float32')
+x_train, x_val, y_train, y_val = train_test_split(x_all, y_all, test_size=0.2, random_state=42)
 
-# Creating PT data samplers and loaders:
-train_sampler = SubsetRandomSampler(train_indices)
-valid_sampler = SubsetRandomSampler(val_indices)
 
-testloader = DataLoader(testset, batch_size=100, shuffle=False, num_workers=10)
+def load_dataset(type='train'):
+    if type == 'train':
+        return x_train, y_train.reshape(-1)
+    elif type == 'val':
+        return x_val, y_val.reshape(-1)
+    elif type == 'test':
+        return x_test, y_test.reshape(-1)
+    elif type == 'all':
+        return x_all, y_all.reshape(-1)
+    else:
+        raise ValueError('Invalid dataset type!')
 
+
+def read_in_all_images(shuffle=True, type='train'):
+    """
+    This function reads all training or validation data, shuffles them if needed, and returns the
+    images and the corresponding labels as numpy arrays
+
+    :param address_list: a list of paths of cPickle files
+    :return: concatenated numpy array of data and labels. Data are in 4D arrays: [num_images,
+    image_height, image_width, image_depth] and labels are in 1D arrays: [num_images]
+    """
+    data, label = load_dataset(type=type)
+    num_data = data.shape[0]
+    if shuffle is True:
+        order = np.random.permutation(num_data)
+        data = data[order, ...]
+        label = label[order]
+
+    data = data.astype(np.float32)
+    return data, label
+
+
+def horizontal_flip(image, axis):
+    '''
+    Flip an image at 50% possibility
+    :param image: a 3 dimensional numpy array representing an image
+    :param axis: 0 for vertical flip and 1 for horizontal flip
+    :return: 3D image after flip
+    '''
+    flip_prop = np.random.randint(low=0, high=2)
+    if flip_prop == 0:
+        image = cv2.flip(image, axis)
+
+    return image
+
+
+def whitening_image(image_np):
+    '''
+    Performs per_image_whitening
+    :param image_np: a 4D numpy array representing a batch of images
+    :return: the image numpy array after whitened
+    '''
+    for i in range(len(image_np)):
+        mean = np.mean(image_np[i, ...])
+        # Use adjusted standard deviation here, in case the std == 0.
+        std = np.max([np.std(image_np[i, ...]), 1.0 / np.sqrt(IMG_HEIGHT * IMG_WIDTH * IMG_DEPTH)])
+        image_np[i, ...] = (image_np[i, ...] - mean) / std
+    return image_np
+
+
+def random_crop_and_flip(batch_data, padding_size):
+    '''
+    Helper to random crop and random flip a batch of images
+    :param padding_size: int. how many layers of 0 padding was added to each side
+    :param batch_data: a 4D batch array
+    :return: randomly cropped and flipped image
+    '''
+    cropped_batch = np.zeros(len(batch_data) * IMG_HEIGHT * IMG_WIDTH * IMG_DEPTH).reshape(
+        len(batch_data), IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH)
+
+    for i in range(len(batch_data)):
+        x_offset = np.random.randint(low=0, high=2 * padding_size, size=1)[0]
+        y_offset = np.random.randint(low=0, high=2 * padding_size, size=1)[0]
+        cropped_batch[i, ...] = batch_data[i, ...][x_offset:x_offset + IMG_HEIGHT,
+                                y_offset:y_offset + IMG_WIDTH, :]
+
+        cropped_batch[i, ...] = horizontal_flip(image=cropped_batch[i, ...], axis=1)
+
+    return cropped_batch
+
+
+def prepare_train_data(padding_size):
+    '''
+    Read all the train data into numpy array and add padding_size of 0 paddings on each side of the
+    image
+    :param padding_size: int. how many layers of zero pads to add on each side?
+    :return: all the train data and corresponding labels
+    '''
+    data, label = read_in_all_images()
+    pad_width = ((0, 0), (padding_size, padding_size), (padding_size, padding_size), (0, 0))
+    data = np.pad(data, pad_width=pad_width, mode='constant', constant_values=0)
+
+    return data, label
+
+
+def prepare_all_data(padding_size):
+    '''
+    Read all the train data into numpy array and add padding_size of 0 paddings on each side of the
+    image
+    :param padding_size: int. how many layers of zero pads to add on each side?
+    :return: all the train data and corresponding labels
+    '''
+    data, label = read_in_all_images(type='all')
+    pad_width = ((0, 0), (padding_size, padding_size), (padding_size, padding_size), (0, 0))
+    data = np.pad(data, pad_width=pad_width, mode='constant', constant_values=0)
+
+    return data, label
+
+
+def read_validation_data():
+    '''
+    Read in validation data. Whitening at the same time
+    :return: Validation image data as 4D numpy array. Validation labels as 1D numpy array
+    '''
+    validation_array, validation_labels = load_dataset(type='val')
+    # print(validation_array.shape, validation_labels.shape)
+    validation_array = whitening_image(validation_array)
+
+    return validation_array, validation_labels
+
+
+def read_test_data():
+    '''
+    Read in test data. Whitening at the same time
+    :return: Test image data as 4D numpy array. Validation labels as 1D numpy array
+    '''
+    test_array, test_labels = load_dataset(type='test')
+    test_array = whitening_image(test_array)
+
+    return test_array, test_labels
--- mfes/evaluate_function/resnet/hyper_parameters.py
+++ mfes/evaluate_function/resnet/hyper_parameters.py
@@ -1,5 +1,9 @@
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
 VALID_BATCH_SIZE = 250
 TEST_BATCH_SIZE = 125
+NUM_RESIDUAL_BLOCKS = 5
 IS_FULL_VALID = True
 TRAIN_EMA_DECAY = 0.95
 TEST_CKPT_PATH = 'model_110.ckpt-79999'
